323a47ea4a718383604b780c7c5e22a51e44cd56
Jonathan Abraham <jonathan.abraham@outlook.com>
2015-11-14 14:02:51 +0100

    First Commit

---
+++ EncryptAES/AESManager.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptAES
+{
+    public class AESManager
+    {
+        #region Attributes and Properties
+
+        public Aes Factory { get; set; }
+        public string Password { get; set; }
+        public byte[] EncryptedBytes { get; set; }
+        public byte[] ClearedBytes { get; set; }
+        public string FilePath { get; set; }
+
+        #endregion
+
+        public void Initialize()
+        {
+            this.Factory = Aes.Create();
+
+        }
+
+        #region Files
+
+        public bool EncryptFile()
+        {
+            try
+            {
+                if (this.Factory == null)
+                    Initialize();
+                this.ClearedBytes = File.ReadAllBytes(this.FilePath);
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(this.Password);
+
+                // Hash the password with SHA256
+                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                byte[] bytesEncrypted = AES_Encrypt(this.ClearedBytes, passwordBytes);
+
+                string fileEncrypted = this.FilePath + "_encrypted";
+
+                File.WriteAllBytes(fileEncrypted, bytesEncrypted);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public bool DecryptFile()
+        {
+            try
+            {
+                byte[] bytesToBeDecrypted = File.ReadAllBytes(this.FilePath);
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(this.Password);
+                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+                string clearFilePath = (this.FilePath.Substring((this.FilePath.Length - 10), 10).Equals("_encrypted")) ?
+                    this.FilePath.Substring(0, this.FilePath.Length - 10) :
+                    Path.GetFileNameWithoutExtension(this.FilePath) + "_decrypted" + Path.GetExtension(this.FilePath);
+                File.WriteAllBytes(clearFilePath, bytesDecrypted);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Strings
+
+        public string EncryptText(string input)
+        {
+            // Get the bytes of the string
+            byte[] bytesToBeEncrypted = Encoding.UTF8.GetBytes(input);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(this.Password);
+
+            // Hash the password with SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            string result = Convert.ToBase64String(bytesEncrypted);
+
+            return result;
+        }
+
+        public string DecryptText(string input)
+        {
+            // Get the bytes of the string
+            byte[] bytesToBeDecrypted = Convert.FromBase64String(input);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(this.Password);
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+            string result = Encoding.UTF8.GetString(bytesDecrypted);
+
+            return result;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        public byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        public byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+
+        #endregion
+    }
+}
---
+++ EncryptMe/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using EncryptAES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EncryptMe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Attributes and Properties
+
+        public AESManager Factory { get; set; }
+
+        #endregion
+
+        #region Initialize
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void InitializeFactory()
+        {
+            Factory = new AESManager();
+            Factory.Initialize();
+        }
+
+        #endregion
+
+        #region Events Catch
+
+        #endregion
+
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            string password = pbPassword.Password;
+            string filePath = tbSource.Text;
+
+            if (this.Factory == null)
+                InitializeFactory();
+            this.Factory.Password = password;
+            this.Factory.FilePath = filePath;
+            if (this.Factory.EncryptFile())
+                lblResult.Content = "Fichier crypté avec succès !";
+        }
+
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            string password = pbPassword.Password;
+            string filePath = tbSource.Text;
+
+            if (this.Factory == null)
+                InitializeFactory();
+            this.Factory.Password = password;
+            this.Factory.FilePath = filePath;
+
+            if (this.Factory.DecryptFile())
+                lblResult.Content = "Fichier décrypté avec succès !";
+        }
+
+        private void btnPath_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                tbSource.Text = filename;
+            }
+        }
+    }
+}
