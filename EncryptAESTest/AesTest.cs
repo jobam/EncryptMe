d0e899490cb901e1b2510cb72d497e5ed3ebd908
Jonathan Abraham <jonathan.abraham@outlook.com>
2017-03-15 16:46:24 +0100

    add test project and gitignore

---
+++ EncryptAESTest/AesTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EncryptAES;
+
+namespace EncryptAESTest
+{
+    [TestClass]
+    public class AesTest
+    {
+        private readonly AESManager aesManager;
+
+        public AesTest()
+        {
+            aesManager = new AESManager();
+            aesManager.Initialize();
+            aesManager.Password = "HelloPassword!=@";
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void EncryptText()
+        {
+            string input = "Hello!-@123 World!";
+            string encrypted = aesManager.EncryptText(input);
+            string decrypted = aesManager.DecryptText(encrypted);
+
+            Assert.AreEqual(input, decrypted);
+        }
+    }
+}
---
+++ EncryptAESTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EncryptAESTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EncryptAESTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee1c9bca-da8e-442f-8151-72488c4b4c12")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
